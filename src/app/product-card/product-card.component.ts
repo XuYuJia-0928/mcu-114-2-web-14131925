1fdd1d9fd4efd556aa5a710bcf81a7ddaf7a0ce9
XuYuJia-0928 <erikxuyujia@gmail.com>
2026-04-19 14:25:07 +0800

    feat(product): 利用事件繫結設定產品顯示狀態

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, booleanAttribute, numberAttribute } from '@angular/core';
+import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -21,11 +21,13 @@ export class ProductCardComponent {
 
   @Input({ transform: booleanAttribute })
   isShow!: boolean;
+  @Output()
+  isShowChange = new EventEmitter<boolean>();
 
   @Input()
   photoUrl!: string;
 
   onSetDisplay(isShow: boolean): void {
-    this.isShow = isShow;
+    this.isShowChange.emit(isShow);
   }
 }
